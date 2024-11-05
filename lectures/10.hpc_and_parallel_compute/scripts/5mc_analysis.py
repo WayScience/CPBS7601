5b8fa8b0219c41ad24b4116013b1d247cf16c96a
MikeLippincott <1michaell2017@gmail.com>
2024-11-05 10:14:12 -0700

    add python script to utils

--- lectures/10.hpc_and_parallel_compute/scripts/5mc_analysis.py
+++ lectures/10.hpc_and_parallel_compute/scripts/5mc_analysis.py
@@ -1,37 +1,11 @@
 import argparse
 import pathlib
+import sys
 
 import pandas as pd
 
-
-def count_5mc_sequences(sequence: str) -> tuple:
-    """
-    This function analyzes a sequence for 5mc content and returns the results.
-
-    Parameters
-    ----------
-    sequence : string
-        a string of DNA sequence
-
-    Returns
-    -------
-    c_count : int
-        the number of 5mc sites in the sequence
-    raw_count : int
-        the total number of C sites in the sequence
-    """
-
-    # cast the sequence to uppercase
-    sequence = sequence.upper()
-    raw_count = 0
-    c_count = 0
-    count_5mc = 0
-    for sequence_element in enumerate(sequence):
-        if sequence_element[1] == "X":
-            c_count += 1
-        raw_count += 1
-
-    return c_count, raw_count, count_5mc
+sys.path.append("../utils/")
+from sequence_analysis import count_5mc_sequences
 
 
 def main():
@@ -44,7 +18,7 @@ def main():
     args = parser.parse_args()
     sequence = args.sequence
     output_file_name = args.output_file_name
-    c_count, raw_count, count_5mc = count_5mc_sequences(sequence)
+    c_count, raw_count, count_5mc, sequence = count_5mc_sequences(sequence)
     # write the results to a file
     output_file = pathlib.Path("../results", output_file_name)
     # make the results directory if it doesn't exist
--- lectures/10.hpc_and_parallel_compute/scripts/multiprocessing_sequence_analysis.py
+++ lectures/10.hpc_and_parallel_compute/scripts/multiprocessing_sequence_analysis.py
@@ -1,39 +1,12 @@
 import multiprocessing
 import pathlib
+import sys
 import time
 
 import pandas as pd
 
-
-def count_5mc_sequences(sequence: str) -> tuple:
-    """
-    This function analyzes a sequence for 5mc content and returns the results.
-
-    Parameters
-    ----------
-    sequence : string
-        a string of DNA sequence
-
-    Returns
-    -------
-    c_count : int
-        the number of 5mc sites in the sequence
-    raw_count : int
-        the total number of C sites in the sequence
-    """
-
-    # cast the sequence to uppercase
-    sequence = sequence.upper()
-    raw_count = 0
-    c_count = 0
-    count_5mc = 0
-    for sequence_element in enumerate(sequence):
-        if sequence_element[1] == "X":
-            c_count += 1
-        raw_count += 1
-
-    return c_count, raw_count, count_5mc, sequence
-
+sys.path.append("../utils/")
+from sequence_analysis import count_5mc_sequences
 
 if __name__ == "__main__":
     # set the path for the input file
